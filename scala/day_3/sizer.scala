13c20e19874ea024f62980b77f926133a079b920
Joe Zack <me@joezack.com>
2013-06-04 23:53:39 -0400

    Almost finished day 3 extra credit, still need to handle exceptions and sum up sizes

--- scala/day_3/sizer.scala
+++ scala/day_3/sizer.scala
@@ -3,8 +3,11 @@ import scala.actors._
 import Actor._
 
 object PageLoader {
+  val linksRegex = "<a\\b+[^>]\\bhref\\b*=\\b*\"([^\"]+)\"" // Only counting hrefs using double-quotes, not worrying about invalid html...not too worried about accuracy for this
+
   def getPageSize(url:String) = Source.fromURL(url).mkString.length
-  def getLinks(url:String) = "<a\\b+".r.findAllIn(Source.fromURL(url).mkString).length
+  def getLinkCounts(url:String) = linksRegex.r.findAllIn(Source.fromURL(url).mkString).length
+  def getLinks(url:String) = linksRegex.r.findAllIn(Source.fromURL(url).mkString).matchData.map(x => x.subgroups(0)).toList
 }
 
 val urls = List("http://www.amazon.com/", "http://www.twitter.com/", "http://www.google.com/", "http://www.cnn.com/")
@@ -23,11 +26,15 @@ def getPageSizeSequentially() = {
 }
 
 def getPageSizeConcurrently() = {
+  getPageSizes(urls)
+}
+
+def getPageSizes(hrefs:List[String]) = {
   val caller = self
-  for(url <- urls) {
+  for(url <- hrefs) {
     actor { caller ! (url, PageLoader.getPageSize(url)) }
   }
-  for(i <- 1 to urls.size) {
+  for(i <- 1 to hrefs.size) {
   	receive {
       case (url, size) => println("Size for " + url + ": " + size)
     }
@@ -37,12 +44,30 @@ def getPageSizeConcurrently() = {
 def getLinkCount() = {
 	val caller = self
 	for(url <- urls) {
-		actor { caller ! (url, PageLoader.getLinks(url)) }
+	  actor { caller ! (url, PageLoader.getLinkCounts(url)) }
 	}
 	for(i <- 1 to urls.size) {
-		receive {
-			case (url, count) => println("Link count for " + url + " " + count)
-		}
+	  receive {
+	    case (url, count) => println("Link count for " + url + ": " + count)
+	  }
+	}
+}
+
+def getPageSizeWithChildren() = {
+	val caller = self
+	for(url <- urls) {
+	  actor { caller ! (url, PageLoader.getLinks(url)) }
+	}
+	for(i <- 1 to urls.size) {
+      receive {
+        case (url, links) => {
+          actor { caller ! ( getPageSizes(links.toString().split(",").toList) ) }// not worrying about absolute v. relative paths or bad way of differentiating hrefs
+        }
+        receive {
+			case (url,count) => println("Size of parent's child links for " + url + " " + count.toString())
+			// TODO need to handle exceptions and sum up sizes
+        }
+      }
 	}
 }
 
@@ -52,3 +77,5 @@ println("Concurrent run")
 timeMethod { getPageSizeConcurrently }
 println("Count lengths")
 timeMethod { getLinkCount }
+println("Concurrent page sizes with children (depth of 1)")
+timeMethod { getPageSizeWithChildren }
